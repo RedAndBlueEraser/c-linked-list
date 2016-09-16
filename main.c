71a274bb199dccd5b930d7341ced8d1c23df5f83
Harry <RedAndBlueEraser@outlook.com>
2016-09-16 18:22:39 +1000

    Initial commit with hello world C program.

---
+++ main.c
@@ -0,0 +1,12 @@
+/*
+ * main.c
+ * Version 20160916
+ * Written by Harry Wong (RedAndBlueEraser)
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
