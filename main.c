cf4972af3006ae65fbea4e029d0b6988f5a7cda1
Harry <RedAndBlueEraser@outlook.com>
2016-09-16 21:53:37 +1000

    Implemented test cases for linked list methods.

--- main.c
+++ main.c
@@ -1,12 +1,60 @@
 /*
  * main.c
- * Version 20160916
+ * Version 20160923
  * Written by Harry Wong (RedAndBlueEraser)
  */
 
 #include <stdio.h>
+#include "linkedlist.h"
+
+int test50(void *data) {
+    return (int)data == 50;
+}
+int test90(void *data) {
+    return (int)data > 0 && (int)data % 45 == 0;
+}
+int test180(void *data) {
+    return (int)data > 0 && (int)data % 60 == 0;
+}
+int testfalse(void *data) {
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+    int i;
+    linkedlist_t l, *list = &l;
+    linkedlist_create(list);
+
+    printf("List size is %d\n", linkedlist_size(list));
+
+    linkedlist_push(list, (void*)30);
+    linkedlist_push(list, (void*)50);
+    linkedlist_push(list, (void*)70);
+    printf("List popped with %d\n", (int)linkedlist_pop(list));
+    linkedlist_push(list, (void*)90);
+    linkedlist_push(list, (void*)180);
+    linkedlist_unshift(list, (void*)20);
+    linkedlist_unshift(list, (void*)10);
+    linkedlist_unshift(list, (void*)0);
+    linkedlist_unshift(list, (void*)-10);
+    printf("List shifted with %d\n", (int)linkedlist_shift(list));
+    printf("List size is %d\n", linkedlist_size(list));
+    for (i = 0; i < linkedlist_size(list); i++) {
+        printf("List %dth item is %d\n", i, linkedlist_get(list, i));
+    }
+    printf("Item 50 is the %dth item in the list counting from 0\n", linkedlist_findindex(list, test50));
+    printf("Item %d in the list is divisible by 45\n", (int)linkedlist_find(list, test90));
+    printf("Item %d in the list is divisible by 60\n", (int)linkedlist_find(list, test180));
+    printf("Unable to find item is fine? %d\n", (int)linkedlist_findindex(list, testfalse) == linkedlist_size(list));
+    linkedlist_shift(list);
+    linkedlist_pop(list);
+    printf("List first item is %d\n", (int)linkedlist_first(list));
+    printf("List last item is %d\n", (int)linkedlist_last(list));
+    printf("List size is %d\n", linkedlist_size(list));
+    for (i = 0; i < linkedlist_size(list); i++) {
+        printf("List %dth item is %d\n", i, linkedlist_get(list, i));
+    }
+
+    linkedlist_destroy(list);
     return 0;
 }
