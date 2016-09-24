502f95e12a52495fb2ed6959cd3ec0a922b246ec
Harry <RedAndBlueEraser@outlook.com>
2016-09-24 17:30:28 +1000

    Simplified test cases for linked list methods.

--- main.c
+++ main.c
@@ -33,12 +33,11 @@ int testfalse(void *data) {
 }
 
 int main(int argc, char *argv[]) {
-    int i;
     linkedlist_t l, *list = &l, l2, *list2 = &l2;
     linkedlist_create(list);
     linkedlist_create(list2);
 
-    printf("List size is %d\n", linkedlist_size(list));
+    printf("List size is %zd\n", linkedlist_size(list));
 
     linkedlist_push(list, (void*)30);
     linkedlist_push(list, (void*)50);
@@ -51,11 +50,8 @@ int main(int argc, char *argv[]) {
     linkedlist_unshift(list, (void*)0);
     linkedlist_unshift(list, (void*)-10);
     printf("List shifted with %d\n", (int)linkedlist_shift(list));
-    printf("List size is %d\n", linkedlist_size(list));
-    for (i = 0; i < linkedlist_size(list); i++) {
-        printf("List %dth item is %d\n", i, linkedlist_get(list, i));
-    }
-    printf("Item 50 is the %dth item in the list counting from 0\n", linkedlist_findindex(list, test50));
+    printf("List: "); linkedlist_printint(list);
+    printf("Item 50 is the %zdth item in the list counting from 0\n", linkedlist_findindex(list, test50));
     printf("Item %d in the list is divisible by 45\n", (int)linkedlist_find(list, test90));
     printf("Item %d in the list is divisible by 60\n", (int)linkedlist_find(list, test180));
     printf("Unable to find item is fine? %d\n", (int)linkedlist_findindex(list, testfalse) == linkedlist_size(list));
@@ -63,30 +59,18 @@ int main(int argc, char *argv[]) {
     linkedlist_pop(list);
     printf("List first item is %d\n", (int)linkedlist_first(list));
     printf("List last item is %d\n", (int)linkedlist_last(list));
-    printf("List size is %d\n", linkedlist_size(list));
-    for (i = 0; i < linkedlist_size(list); i++) {
-        printf("List %dth item is %d\n", i, linkedlist_get(list, i));
-    }
+    printf("List: "); linkedlist_printint(list);
     printf("List 2 is empty? %d\n", linkedlist_isempty(list2));
     linkedlist_clone(list, list2);
-    printf("List size is %d\n", linkedlist_size(list));
-    for (i = 0; i < linkedlist_size(list); i++) {
-        printf("List %dth item is %d\n", i, linkedlist_get(list, i));
-    }
+    printf("List: "); linkedlist_printint(list);
     printf("List is empty? %d\n", linkedlist_isempty(list));
     linkedlist_destroy(list);
     printf("List is empty? %d\n", linkedlist_isempty(list));
-    printf("List 2 size is %d\n", linkedlist_size(list2));
-    for (i = 0; i < linkedlist_size(list2); i++) {
-        printf("List 2 %dth item is %d\n", i, linkedlist_get(list2, i));
-    }
+    printf("List 2: "); linkedlist_printint(list2);
     printf("List 2 is empty? %d\n", linkedlist_isempty(list2));
     linkedlist_destroy(list2);
     printf("List 2 is empty? %d\n", linkedlist_isempty(list2));
-    for (i = 0; i < linkedlist_size(list); i++) {
-        printf("List %dth item is %d\n", i, linkedlist_get(list, i));
-    }
-
+    printf("List 2: "); linkedlist_printint(list2);
     printf("Unable to pop item is fine? %d\n", linkedlist_pop(list) == NULL);
     printf("Unable to shift item is fine? %d\n", linkedlist_shift(list) == NULL);
     printf("Unable to remove item is fine? %d\n", linkedlist_remove(list, 0) == NULL);
